e50041be717fd2f8e2b829723f1922c557bf7b54
possibly <137134852+possibIy@users.noreply.github.com>
2024-08-27 09:16:43 -0400

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://piped.video/";//your website url
+  let target = "https://tube.cadence.moe/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
